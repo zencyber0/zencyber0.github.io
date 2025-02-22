22de623b0c9934d6d8e367ea5583ff86d5ef16d3
zencyber <139611438+zencyber0@users.noreply.github.com>
2025-02-21 19:08:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-fetch('http://ip-api.com/json', {
-  method: 'POST', // or 'POST', 'PUT', 'DELETE'
-  headers: {
-    'Content-Type': 'application/json'
-  },
-})
-.then(response => {
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  return response.json(); 
-})
-.then(data => {
-  console.log('Data received:', data);
-})
-.catch(error => {
-  console.error('There was an error:', error);
-});
+<script>alert("success")</script>
