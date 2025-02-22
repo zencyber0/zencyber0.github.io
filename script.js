ec4ebecaecea48a6a52e873b2e1a63c26f611150
zencyber <139611438+zencyber0@users.noreply.github.com>
2025-02-21 21:10:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 fetch('https://api64.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(`Your IP Address: ${data.ip}`);
-    
-    return fetch(`https://ipapi.co/${data.ip}/json/`);
-  })
-  .then(response => response.json())
-  .then(details => {
-    console.log('IP Details:', details);
-  })
-  .catch(error => console.error('Error fetching IP details:', error));
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
