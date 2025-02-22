db682b298cff35124a32ed3d8339f172d9c1b88a
zencyber <139611438+zencyber0@users.noreply.github.com>
2025-02-21 19:04:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert(0);
+fetch('http://ip-api.com/json', {
+  method: 'GET', // or 'POST', 'PUT', 'DELETE'
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ key: 'value' }) // Only for methods with a body (POST, PUT)
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return response.json(); 
+})
+.then(data => {
+  console.log('Data received:', data);
+})
+.catch(error => {
+  console.error('There was an error:', error);
+});
