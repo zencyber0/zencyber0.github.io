a758df060a88d935769a8dde47b7e4f67b585aff
zencyber <139611438+zencyber0@users.noreply.github.com>
2025-02-21 21:50:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ var Online = 0;
 if (Online == 0) {
     var Online = 1;
 }
-console.log("Users Online ->>", Online)
+console.log("{+} Users Online -->", Online)
 
 
 setTimeout(() => {
 fetch('https://api64.ipify.org?format=json')
     .then(response => response.json())
     .then(data => console.log(data));},250);
+    .then(console.log(data))
 setTimeout(() => {
 const message = confirm("Tripwire Tripped!")
 },500);
