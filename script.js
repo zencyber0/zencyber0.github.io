125dd8b9af04323446a7bc43401697851444983d
zencyber <139611438+zencyber0@users.noreply.github.com>
2025-02-21 21:40:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 setTimeout(() => {
 fetch('https://api64.ipify.org?format=json')
     .then(response => response.json())
-    .then(console.log("ip is"))
     .then(data => console.log(data));},2000);
+const message = confirm("IP Fetched!")
