fb769a341d04b15553eb6e3fc9d46d944f49e2c6
zencyber <139611438+zencyber0@users.noreply.github.com>
2025-02-21 21:05:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ console.log("{+} script.js Loaded!")
 console.log("{+} index.html Loaded!")
 console.log("{+} style.css Loaded!")
 },2000)
+
+fetch('http://ip-api.com/json')
+  .then(response => response.json())
+  .then(data => console.log(data));
