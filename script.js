cd3f80a826dcdb19b5b370a8cd0fe80df15fc271
zencyber <139611438+zencyber0@users.noreply.github.com>
2025-02-21 21:13:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
+setTimeout(() => {
 fetch('https://api64.ipify.org?format=json')
     .then(response => response.json())
     .then(console.log("ip is"))
-    .then(data => console.log(data));
+    .then(data => console.log(data));},2000);
